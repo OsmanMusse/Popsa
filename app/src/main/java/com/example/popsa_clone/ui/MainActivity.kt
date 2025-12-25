f2f7ea9e8cb4cf6d5926f279d144d3ed9f123727
Osman <Namsomata@gmail.com>
2025-12-25 21:53:57 +0000

    update navigationBar color from White to Black

--- app/src/main/java/com/example/popsa_clone/ui/MainActivity.kt
+++ app/src/main/java/com/example/popsa_clone/ui/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.popsa_clone.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.example.popsa_clone.PopsaNavHost
 import com.example.popsa_clone.ui.theme.PopsaCloneTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +16,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.dark(Color.Black.toArgb())
+        )
         setContent {
             PopsaCloneTheme {
                 PopsaNavHost()
