5f18849f950465e3462478c6c481f4d728a70781
Osman <Namsomata@gmail.com>
2025-12-25 21:55:06 +0000

    add emphasized(bold logic) when viewing productOptionsCard

--- app/src/main/java/com/example/popsa_clone/ui/home/components/FromPriceText.kt
+++ app/src/main/java/com/example/popsa_clone/ui/home/components/FromPriceText.kt
@@ -1,9 +1,10 @@
 package com.example.popsa_clone.ui.home.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,14 +21,8 @@ enum class PriceTextSize(
     val originalPriceSize: TextUnit,
     val fromPriceSize: TextUnit
 ) {
-    FEATURED(
-        originalPriceSize = 17.sp,
-        fromPriceSize = 17.sp
-    ),
-    GRID(
-        originalPriceSize = 12.sp,
-        fromPriceSize = 14.sp
-    )
+    FEATURED(originalPriceSize = 15.5.sp, fromPriceSize = 16.5.sp),
+    GRID(originalPriceSize = 12.sp, fromPriceSize = 14.sp),
 }
 
 @Composable
@@ -35,16 +30,20 @@ fun FromPriceText(
     originalPrice: Double,
     fromPrice: Double,
     priceTextSize: PriceTextSize,
-    modifier: Modifier = Modifier
+    emphasized: Boolean = false
 ) {
-    Column(modifier = modifier) {
+
+    val fromText = if (emphasized) "FROM" else "From"
+    Column {
         Text(
-            text = "From",
+            text = fromText,
             fontWeight = FontWeight.Medium,
-            fontSize = 14.5.sp,
-            lineHeight = 14.5.sp,
-            color = Color.Gray,
+            fontSize = if (emphasized) 12.sp else 13.sp,
+            lineHeight = 13.sp,
+            color = if (emphasized) Color.Black else Color.Gray,
         )
+
+        if (emphasized) Spacer(Modifier.height(3.dp))
 
         Row(
             horizontalArrangement = Arrangement.spacedBy(3.dp)
